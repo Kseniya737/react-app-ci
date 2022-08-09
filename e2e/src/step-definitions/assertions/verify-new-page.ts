7ec12137d4f0504b90791aa9559ad08e2db27031
Kseniya Maslova <ksenya-37@mail.ru>
2022-08-09 16:56:40 +0200

    completed lessons 71-73

--- e2e/src/step-definitions/assertions/verify-new-page.ts
+++ e2e/src/step-definitions/assertions/verify-new-page.ts
@@ -16,7 +16,7 @@ Then(
             screen: { page, context },
         } = this;
 
-        console.log(`the ${elementPosition} tab should ${negate?'not ':'' }contain the title ${expectedTitle}`);
+        console.log(`the ${elementPosition} window|tab should ${negate?'not ':'' }contain the title ${expectedTitle}`);
 
         const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1;
 
@@ -29,3 +29,66 @@ Then(
         });
     }
 );
+
+Then(
+    /^the "([^"]*)" on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? be displayed$/,
+    async function (this: ScenarioWorld, elementKey: ElementKey, elementPosition: string, negate: boolean){
+        const {
+            screen: { page, context },
+            globalConfig
+        } = this;
+
+        console.log(`the ${elementKey} on the ${elementPosition} window|tab should ${negate?' not':''} be visible`)
+
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1;
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async() => {
+            let pages = context.pages();
+            const isElementVisible = (await pages[pageIndex].$(elementIdentifier)) !== null;
+            return isElementVisible === !negate;
+        });
+    }
+)
+
+Then(
+    /^the "([^"]*)" on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? contain the text "(.*)"$/,
+    async function (this: ScenarioWorld, elementKey: ElementKey, elementPosition: string, negate: boolean, expectedElementText: string){
+        const {
+            screen: { page, context },
+            globalConfig
+        } = this;
+
+        console.log(`the ${elementKey} on the ${elementPosition} window|tab should ${negate?' not':''} contain the text ${expectedElementText}`)
+
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1;
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            let pages = context.pages();
+            const elementText = await pages[pageIndex].textContent(elementIdentifier);
+            return elementText?.includes(expectedElementText) === !negate;
+        })
+    }
+)
+
+Then(
+    /^the "([^"]*)" on the "([0-9]+th|[0-9]+st|[0-9]+nd|[0-9]+rd)" (?:tab|window) should( not)? equal the text "(.*)"$/,
+    async function (this: ScenarioWorld, elementKey: ElementKey, elementPosition: string, negate: boolean, expectedElementText: string){
+        const {
+            screen: { page, context },
+            globalConfig
+        } = this;
+
+        console.log(`the ${elementKey} on the ${elementPosition} window|tab should ${negate?' not':''} equal the text ${expectedElementText}`)
+
+        const pageIndex = Number(elementPosition.match(/\d/g)?.join('')) - 1;
+        const elementIdentifier = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor(async () => {
+            let pages = context.pages();
+            const elementText = await pages[pageIndex].textContent(elementIdentifier);
+            return (elementText === expectedElementText) === !negate;
+        })
+    }
+)
