7448a38e8dbbddd290f805b3ba8bb8456a82652b
Kseniya Maslova <ksenya-37@mail.ru>
2022-08-16 10:55:38 +0200

    fixed run_tests script for Windows.

--- e2e/run_tests.bat
+++ e2e/run_tests.bat
@@ -2,6 +2,6 @@ set env=%1
 set tag=%2
 
 set COMMON_CONFIG_FILE=env/common.env
-sen NODE_END=%env%
+set NODE_ENV=%env%
 
 yarn run cucumber_windows:%env% --profile %tag% || yarn run postcucumber_windows
