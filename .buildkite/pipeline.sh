1957f3c8dc66a88dd706c9e72fd6ff736fef5dc6
Kseniya Maslova <ksenya-37@mail.ru>
2022-08-19 13:09:30 +0200

    fixed pipeline script

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 PIPELINE=${PIPELINE:-build}
 
-function build(){
-  cat <<EQF
+function build() {
+  cat <<EOF
 steps:
   - name: ":desktop_computer: Automation"
-  command: "./scripts/buildkite-automation.sh"
-EQF
+    command: "./scripts/buildkite-automation.sh"
+EOF
 }
 
 $PIPELINE
